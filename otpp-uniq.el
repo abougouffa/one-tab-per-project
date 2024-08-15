9f1828db94952b88796a14396aad5c36e0ff97ab
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-08-15 17:05:28 +0200

    chore(version): v3.0.0

--- otpp-uniq.el
+++ otpp-uniq.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/abougouffa/one-tab-per-project
 ;; Created: July 07, 2024
 ;; Modified: August 15, 2024
-;; Version: 2.0.0
+;; Version: 3.0.0
 ;; Package-Requires: ((emacs "28.1") (compat "29.1"))
 ;; SPDX-License-Identifier: GPL-3.0
 
