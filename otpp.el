43a4928543c53c5da5334bf0de39550830484943
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-06-14 23:37:46 +0200

    chore(version): v3.3.0

--- otpp.el
+++ otpp.el
@@ -1,12 +1,12 @@
 ;;; otpp.el --- One tab per project, with unique names -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2024  Abdelhak Bougouffa
+;; Copyright (C) 2024-2025  Abdelhak Bougouffa
 
 ;; Author: Abdelhak Bougouffa (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; URL: https://github.com/abougouffa/one-tab-per-project
 ;; Created: July 07, 2024
-;; Modified: April 14, 2025
-;; Version: 3.2.1
+;; Modified: June 14, 2025
+;; Version: 3.3.0
 ;; Package-Requires: ((emacs "28.1") (compat "29.1"))
 ;; Keywords: convenience
 ;; SPDX-License-Identifier: GPL-3.0
